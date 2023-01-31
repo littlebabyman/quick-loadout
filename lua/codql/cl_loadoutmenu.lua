03d82db0fcbb49e2544a0ea8992f2eea80242e1e
littlebabyman <3816236+littlebabyman@users.noreply.github.com>
2023-01-31 16:29:33 +0200

    gREAT SUCCESS

--- lua/codql/cl_loadoutmenu.lua
+++ lua/codql/cl_loadoutmenu.lua
@@ -2,6 +2,24 @@ AddCSLuaFile()
 CreateClientConVar("codql_weapons", "", true, true, "Quick loadout weapon classes.")
 local weaponlist = GetConVar("codql_weapons")
 
+local function GenerateButton(frame, i, v, off)
+    local button = vgui.Create("DButton", frame)
+    local text = v or "ASS"
+    button:SetWrap(true)
+    button:SetText(text)
+    button:SetWidth(frame:GetWide() - frame:GetVBar():GetWide() - 1)
+    button:SetHeight(20)
+    button:SetPos(0, off)
+    return button
+end
+
+local function GenerateCategory(frame)
+    local category = vgui.Create("DScrollPanel", frame)
+    category:SetWidth(200)
+    category:Dock(2)
+    return category
+end
+
 function QLOpenMenu()
     local mainmenu = vgui.Create("DFrame")
     mainmenu:SetPos(ScrW() / 2-320, ScrH() / 2-240)
@@ -11,23 +29,12 @@ function QLOpenMenu()
     mainmenu:SetDraggable(false)
     mainmenu:ShowCloseButton(true)
     mainmenu:MakePopup()
-    local weplist = vgui.Create("DScrollPanel", mainmenu)
-    local weplistbar = weplist:GetVBar():GetWide()
-    weplist:SetWidth(200)
-    weplist:Dock(2)
-    local category = vgui.Create("DScrollPanel", mainmenu)
-    local catbar = category:GetVBar():GetWide()
-    category:SetWidth(200)
-    category:Dock(2)
-    local subcat = vgui.Create("DScrollPanel", mainmenu)
-    local subcatbar = subcat:GetVBar():GetWide()
-    subcat:SetWidth(200)
-    subcat:Dock(2)
+    weplist = GenerateCategory(mainmenu)
     local ptable = string.Explode(" ", GetConVar("codql_weapons"):GetString())
     PrintTable(ptable)
     local wtable = {}
     for k, v in SortedPairs(list.Get( "Weapon" )) do
-        if v.Spawnable then
+        if v.Spawnable and (!v.AdminOnly or LocalPlayer():IsSuperAdmin()) then
             if !wtable[v.Category] then
                 wtable[v.Category] = {}
             end
@@ -36,15 +43,6 @@ function QLOpenMenu()
     end
     -- PrintTable(wtable)
     -- print("Table printed!")
-    local function GenerateButton(frame, i, v, off)
-        local button = vgui.Create("DButton", frame)
-        button:SetWrap(true)
-        button:SetText(v or "ASS")
-        button:SetWidth(frame:GetWide() - frame:GetVBar():GetWide() - 1)
-        button:SetHeight(20)
-        button:SetPos(0, off)
-        return button
-    end
     local offset = 0
     local function WepSelector(index, wep)
         print(wep)
@@ -52,11 +50,13 @@ function QLOpenMenu()
         offset = offset + weapon:GetTall()
         weapon.DoClick = function()
             offset = 0
+            category = GenerateCategory(mainmenu)
             for k, _ in SortedPairs(wtable) do
                 button = GenerateButton(category, index, k, offset)
                 offset = offset + button:GetTall()
                 button.DoClick = function()
                     offset = 0
+                    subcat = GenerateCategory(mainmenu)
                     for i, v in SortedPairs(wtable[k]) do
                         subbutton = GenerateButton(subcat, index, v, offset)
                         offset = offset + subbutton:GetTall()
@@ -64,13 +64,15 @@ function QLOpenMenu()
                             table.Merge(ptable, {[index] = i})
                             weapon:SetText(v .. " (" .. k .. ")")
                             -- PrintTable(ptable)
+                            subcat:Remove()
+                            category:Remove()
                         end
                     end
                 end
             end
         end
-        if IsValid(wep) then
-            weapon.DoRightClick = function()
+        weapon.DoRightClick = function()
+            if table.HasValue(ptable, wep) then
                 table.remove(ptable, index)
                 weapon:Remove()
             end
@@ -79,7 +81,7 @@ function QLOpenMenu()
     for i, v in ipairs(ptable) do
         WepSelector(i, v)
     end
-    WepSelector(#ptable + 1, "Add Weapon")
+    -- WepSelector(#ptable + 1, "Add Weapon")
     mainmenu.OnClose = function()
         print(table.concat(ptable," "))
         weaponlist:SetString(table.concat(ptable, " "))
