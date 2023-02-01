8f10fba126a717ed06091315cff1245fa4243157
littlebabyman <3816236+littlebabyman@users.noreply.github.com>
2023-02-02 00:03:25 +0200

    default 0

--- lua/quickloadout/sh_cvars.lua
+++ lua/quickloadout/sh_cvars.lua
@@ -1,6 +1,6 @@
 AddCSLuaFile()
 CreateConVar("quickloadout_enable", 1, {FCVAR_ARCHIVE + FCVAR_REPLICATED + FCVAR_NOTIFY}, "Enable Quick Loadout.", 0, 1)
-CreateConVar("quickloadout_override", 1, {FCVAR_ARCHIVE + FCVAR_REPLICATED + FCVAR_NOTIFY}, "Override default loadout.", 0, 1)
+CreateConVar("quickloadout_override", 0, {FCVAR_ARCHIVE + FCVAR_REPLICATED + FCVAR_NOTIFY}, "Override default loadout.", 0, 1)
 CreateConVar("quickloadout_maxslots", 5, {FCVAR_ARCHIVE + FCVAR_REPLICATED + FCVAR_NOTIFY}, "Max weapon slots in a loadout.", 0, 8^8)
 if CLIENT then
     CreateClientConVar("quickloadout_weapons", "", true, true, "Quick loadout weapon classes.")
