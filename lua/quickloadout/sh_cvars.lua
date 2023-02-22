89ee3b04a6fdad85ef0ab2b87e2f61aa0a25b180
littlebabyman <3816236+littlebabyman@users.noreply.github.com>
2023-02-22 21:41:01 +0200

    defautl loadout player-defined

--- lua/quickloadout/sh_cvars.lua
+++ lua/quickloadout/sh_cvars.lua
@@ -1,6 +1,6 @@
 AddCSLuaFile()
 CreateConVar("quickloadout_enable", 1, {FCVAR_ARCHIVE + FCVAR_REPLICATED + FCVAR_NOTIFY}, "Enable Quick Loadout.", 0, 1)
-CreateConVar("quickloadout_default", 0, {FCVAR_ARCHIVE + FCVAR_REPLICATED + FCVAR_NOTIFY}, "Enable default loadout.", -1, 1)
+CreateConVar("quickloadout_default", -1, {FCVAR_ARCHIVE + FCVAR_REPLICATED + FCVAR_NOTIFY}, "Enable default loadout.", -1, 1)
 CreateConVar("quickloadout_maxslots", 5, {FCVAR_ARCHIVE + FCVAR_REPLICATED + FCVAR_NOTIFY}, "Max weapon slots in a loadout.", 0, 32)
 CreateConVar("quickloadout_switchtime", 15, {FCVAR_ARCHIVE + FCVAR_REPLICATED + FCVAR_NOTIFY}, "Grace period length to switch loadout after spawn.", 0)
 CreateConVar("quickloadout_switchtime_override", 0, {FCVAR_ARCHIVE + FCVAR_REPLICATED + FCVAR_NOTIFY}, "Force grace period off on pressing primary attack.", 0, 1)
