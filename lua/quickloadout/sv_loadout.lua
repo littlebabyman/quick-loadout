c31e7c9a816326141ca81cb7b6a6212bc18c5b43
littlebabyman <3816236+littlebabyman@users.noreply.github.com>
2023-02-25 18:19:13 +0200

    removed anti-crash (it works now)

--- lua/quickloadout/sv_loadout.lua
+++ lua/quickloadout/sv_loadout.lua
@@ -44,7 +44,6 @@ function QuickLoadout(ply)
     if default:GetInt() == 1 or (default:GetInt() == -1 and ply:GetInfoNum("quickloadout_default_client", 1) == 1) or table.IsEmpty(ply.quickloadout) then hook.Run("PlayerLoadout", ply) end
     for k, v in ipairs(ply.quickloadout) do
         if !maxslots:GetBool() or maxslots:GetInt() >= k then
-            if k == 1 and weapons.Get(v) and weapons.Get(v).ARC9 then ply:PrintMessage(HUD_PRINTCENTER, "ARC9 SWEP prevented from crashing the game. Please don't set it as your first weapon!") v = "weapon_stunstick" end
             ply:Give(v)
         end
     end
